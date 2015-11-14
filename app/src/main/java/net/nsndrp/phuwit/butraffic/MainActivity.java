66da6e888432d46ce3fd8ce639f94c78d2c2676e
Phuwit Ninsomnuek <phuwit.nins@nsndrp.net>
2015-11-14 15:00:38 +0700

    sound effect

--- app/src/main/java/net/nsndrp/phuwit/butraffic/MainActivity.java
+++ app/src/main/java/net/nsndrp/phuwit/butraffic/MainActivity.java
@@ -1,13 +1,55 @@
 package net.nsndrp.phuwit.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /*    Explicit     */
+    private ListView trafficListView;
+    private Button aboutmeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+
+        bindWidget();
+        /*Button Controller*/
+
+        buttonController();
+
+
+
+
+
+
+    } // Main method
+
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(),R.raw.dog);
+                buttonPlayer.start();
+
+                // Inten
+
+            } // Event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+    }
+}   // Main Class
